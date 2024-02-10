da89eb37b178c69bbe476e453f765cf265e41905
David Hagen <david@drhagen.com>
2024-02-10 05:44:09 -0500

    Initial port from main repo

---
+++ noxfile.py
@@ -0,0 +1,31 @@
+import platform
+
+from nox import Session, options, parametrize, session
+
+options.sessions = ["test", "coverage", "lint"]
+
+
+@session(python=["3.10", "3.11", "3.12"])
+def test(s: Session):
+    s.install(".", "pytest", "pytest-cov")
+    s.env["COVERAGE_FILE"] = f".coverage.{s.python}.{platform.system()}"
+    s.run("python", "-m", "pytest", "--cov", "tensora_taco", "tests")
+
+
+@session(venv_backend="none")
+def coverage(s: Session):
+    s.run("coverage", "combine")
+    s.run("coverage", "html")
+    s.run("coverage", "xml", "--fail-under=100")
+
+
+@session(venv_backend="none")
+@parametrize("command", [["ruff", "check", "."], ["ruff", "format", "--check", "."]])
+def lint(s: Session, command: list[str]):
+    s.run(*command)
+
+
+@session(venv_backend="none")
+def format(s: Session) -> None:
+    s.run("ruff", "check", ".", "--select", "I", "--fix")
+    s.run("ruff", "format", ".")
